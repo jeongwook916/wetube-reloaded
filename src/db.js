d6378ba357f68567a4864f500825beb7ee10cf97
jeongwook916 <110967890+jeongwook916@users.noreply.github.com>
2023-01-17 16:46:33 +0900

    2.4.1

--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
 
-const db = mongoose.conection;
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log("❌ DB Error", error);
