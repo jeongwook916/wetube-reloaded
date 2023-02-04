47d8bd8a148913f3cff78df1dfaa1f9df07aab54
jeongwook916 <110967890+jeongwook916@users.noreply.github.com>
2023-02-04 14:33:09 +0900

    4.1

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
+console.log(process.env.DB_URL);
+
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
